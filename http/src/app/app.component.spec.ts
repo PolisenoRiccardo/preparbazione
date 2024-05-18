8be407b71425ce666caa8174e41327b21811f0c3
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2024-05-18 22:49:33 +0000

    POST

--- http/src/app/app.component.spec.ts
+++ http/src/app/app.component.spec.ts
@@ -15,7 +15,6 @@ describe('AppComponent', () => {
   it(`should have as title 'http'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('http');
   });
 
   it('should render title', () => {
--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -10,8 +10,11 @@ import { Prenotazione } from './prenotazione.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  observPrenotazione !:  Observable<Prenotazione>;
   observPrenotazioneArray !: Observable<Prenotazione[]>;
   prenotazioni : Prenotazione[] = [];
+  nuovaPrenotazione !: Prenotazione;
+  counter : number = 0;
  
   constructor(public http: HttpClient) {
     this.makeTypedRequest()
@@ -21,7 +24,30 @@ export class AppComponent {
   {
     this.observPrenotazioneArray = this.http.get<Prenotazione[]>('https://my-json-server.typicode.com/PolisenoRiccardo/fakeServer/prenotazioni');
     this.observPrenotazioneArray.subscribe(prenotazioni => {this.prenotazioni = prenotazioni;}); // Vengono salvati i dati ricevuti con il get, nella variabile prenotazioni.
-
   }
 
+  prenota(
+          newNome:HTMLInputElement, 
+          newCognome:HTMLInputElement, 
+          newIndirizzo:HTMLInputElement, 
+          newTelefono:HTMLInputElement, 
+          newMail:HTMLInputElement, 
+          newData:HTMLInputElement, 
+          newOra:HTMLInputElement): boolean 
+        {
+          this.nuovaPrenotazione = new Prenotazione(newNome.value, newCognome.value, newIndirizzo.value, newTelefono.value, newMail.value, newData.value, newOra.value);
+          this.prenotazioni.push(this.nuovaPrenotazione)
+          this.makePost()
+          this.counter = this.counter + 1
+          console.log(this.counter)
+          return false;
+        }
+    
+  makePost() {
+    let posted = JSON.stringify(this.nuovaPrenotazione);
+    const headers = {'Content-Type': 'application/json', 'My-Custom-Header': 'foobar' };
+    this.observPrenotazione = this.http.post<Prenotazione>("https://my-json-server.typicode.com/PolisenoRiccardo/fakeServer/prenotazioni", posted, {headers});
+    this.observPrenotazione.subscribe(postPrenotazione => {this.nuovaPrenotazione = postPrenotazione;});
+  }
+  
 }
